0fa3d76d2b5f204df492317931367fe97a927e65
Dan Vega <danvega@gmail.com>
2020-02-20 22:23:37 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      height: {
+        350: "350px",
+        400: "400px",
+        550: "550px"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
